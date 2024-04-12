efd620163265ac12d5b02e1d9776d932e3caaf08
cagrisayir <omercagris@gmail.com>
2024-04-12 11:56:34 +0300

    Kisi kayit

--- app/src/main/java/dev/cagrisayir/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
+++ app/src/main/java/dev/cagrisayir/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import dev.cagrisayir.kisileruygulamasi.R
 import dev.cagrisayir.kisileruygulamasi.databinding.FragmentAnasayfaBinding
 
 class AnasayfaFragment : Fragment() {
@@ -16,6 +19,13 @@ class AnasayfaFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentAnasayfaBinding.inflate(inflater, container, false)
+
+        binding.fab.setOnClickListener {
+            val navController = Navigation.findNavController(it)
+            navController.navigate(R.id.kisiKayitGecis)
+        }
+
+
         return binding.root
     }
 
--- app/src/main/java/dev/cagrisayir/kisileruygulamasi/ui/fragment/KisiKayitFragment.kt
+++ app/src/main/java/dev/cagrisayir/kisileruygulamasi/ui/fragment/KisiKayitFragment.kt
@@ -1,10 +1,14 @@
 package dev.cagrisayir.kisileruygulamasi.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import dev.cagrisayir.kisileruygulamasi.databinding.FragmentKisiKayitBinding
 
 class KisiKayitFragment : Fragment() {
@@ -16,6 +20,32 @@ class KisiKayitFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentKisiKayitBinding.inflate(inflater, container, false)
+
+        binding.toolbarKisiKayit.title = "Kisi Kayit"
+
+        binding.buttonKaydet.setOnClickListener {
+            val kisi_ad = binding.editTextKisiAd.text.toString()
+            val kisi_tel = binding.editTextKisiTel.text.toString()
+            kaydet(kisi_ad, kisi_tel)
+            temizle()
+            hideTheKeyboard(it)
+        }
+
         return binding.root
+    }
+
+    private fun kaydet(kisi_ad: String, kisi_tel: String) {
+        Log.e("Kisi Kaydet", "$kisi_ad - $kisi_tel")
+        Toast.makeText(context, "Kisi kayit edildi: $kisi_ad", Toast.LENGTH_LONG).show()
+    }
+
+    private fun temizle() {
+        binding.editTextKisiAd.setText("")
+        binding.editTextKisiTel.setText("")
+    }
+
+    private fun hideTheKeyboard(view: View) {
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
