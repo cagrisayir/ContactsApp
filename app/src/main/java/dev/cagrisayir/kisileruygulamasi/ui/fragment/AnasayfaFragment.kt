514b917a6bc52462bd865273fba5cbe93ca30683
cagrisayir <omercagris@gmail.com>
2024-04-12 12:24:23 +0300

    kisi detay

--- app/src/main/java/dev/cagrisayir/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
+++ app/src/main/java/dev/cagrisayir/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import dev.cagrisayir.kisileruygulamasi.R
+import dev.cagrisayir.kisileruygulamasi.data.entity.Kisiler
 import dev.cagrisayir.kisileruygulamasi.databinding.FragmentAnasayfaBinding
 
 class AnasayfaFragment : Fragment() {
@@ -19,12 +20,18 @@ class AnasayfaFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentAnasayfaBinding.inflate(inflater, container, false)
-
+        binding.toolbarAnasayfa.title = "Anasayfa"
+        binding.toolbarAnasayfa.setTitleTextColor(getResources().getColor(R.color.white))
         binding.fab.setOnClickListener {
             val navController = Navigation.findNavController(it)
             navController.navigate(R.id.kisiKayitGecis)
         }
 
+        binding.buttonDetay.setOnClickListener {
+            val kisi = Kisiler(1, "Ahmet", "1111")
+            val gecis = AnasayfaFragmentDirections.kisiDetayGecis(kisi)
+            Navigation.findNavController(it).navigate(gecis)
+        }
 
         return binding.root
     }
--- app/src/main/java/dev/cagrisayir/kisileruygulamasi/ui/fragment/KisiDetayFragment.kt
+++ app/src/main/java/dev/cagrisayir/kisileruygulamasi/ui/fragment/KisiDetayFragment.kt
@@ -1,10 +1,17 @@
 package dev.cagrisayir.kisileruygulamasi.ui.fragment
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.navigation.fragment.navArgs
+import dev.cagrisayir.kisileruygulamasi.R
 import dev.cagrisayir.kisileruygulamasi.databinding.FragmentKisiDetayBinding
 
 class KisiDetayFragment : Fragment() {
@@ -16,6 +23,31 @@ class KisiDetayFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentKisiDetayBinding.inflate(inflater, container, false)
+
+        binding.toolbarKisiDetay.title = "Kisi Detay"
+        binding.toolbarKisiDetay.setTitleTextColor(getResources().getColor(R.color.white))
+
+        val bundle: KisiDetayFragmentArgs by navArgs()
+        val gelenKisi = bundle.kisi
+
+        binding.kisiAdText.setText(gelenKisi.kisi_ad)
+        binding.kisiTelText.setText(gelenKisi.kisi_tel)
+
+        binding.buttonGuncelle.setOnClickListener {
+            kaydet(kisi_id = gelenKisi.kisi_id, kisi_ad = gelenKisi.kisi_ad, kisi_tel = gelenKisi.kisi_tel)
+            hideTheKeyboard(it)
+        }
+
         return binding.root
+    }
+
+    private fun kaydet(kisi_id: Int, kisi_ad: String, kisi_tel: String) {
+        Log.e("Kisi Guncellendi", "$kisi_ad - $kisi_tel")
+        Toast.makeText(context, "Kisi Guncellendi: $kisi_id: $kisi_ad", Toast.LENGTH_LONG).show()
+    }
+
+    private fun hideTheKeyboard(view: View) {
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
