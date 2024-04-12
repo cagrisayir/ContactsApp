35811e6f0815a4a37086d31d0eb77922bfb9e9d9
cagrisayir <omercagris@gmail.com>
2024-04-12 10:09:39 +0300

    Initial commit

---
+++ app/src/main/java/dev/cagrisayir/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
@@ -0,0 +1,22 @@
+package dev.cagrisayir.kisileruygulamasi.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.cagrisayir.kisileruygulamasi.databinding.FragmentAnasayfaBinding
+
+class AnasayfaFragment : Fragment() {
+    private lateinit var binding: FragmentAnasayfaBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentAnasayfaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/dev/cagrisayir/kisileruygulamasi/ui/fragment/KisiKayitFragment.kt
@@ -0,0 +1,21 @@
+package dev.cagrisayir.kisileruygulamasi.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.cagrisayir.kisileruygulamasi.databinding.FragmentKisiKayitBinding
+
+class KisiKayitFragment : Fragment() {
+    private lateinit var binding: FragmentKisiKayitBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentKisiKayitBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
