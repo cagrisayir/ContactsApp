7ec7f72e9dd1b39d60c07092783bd823173427a1
cagrisayir <omercagris@gmail.com>
2024-04-12 16:06:08 +0300

    card and recycler view

---
+++ app/src/main/java/dev/cagrisayir/kisileruygulamasi/ui/adapter/KisilerAdapter.kt
@@ -0,0 +1,51 @@
+package dev.cagrisayir.kisileruygulamasi.ui.adapter
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import dev.cagrisayir.kisileruygulamasi.data.entity.Kisiler
+import dev.cagrisayir.kisileruygulamasi.databinding.CardTasarimBinding
+import dev.cagrisayir.kisileruygulamasi.ui.fragment.AnasayfaFragmentDirections
+
+class KisilerAdapter(var mContext: Context, var kisilerListesi: List<Kisiler>): RecyclerView.Adapter<KisilerAdapter.CardTasarimTutucu>() {
+    inner class CardTasarimTutucu(var tasarim: CardTasarimBinding): RecyclerView.ViewHolder(tasarim.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardTasarimTutucu {
+        val binding = CardTasarimBinding.inflate(LayoutInflater.from(mContext), parent, false)
+        return CardTasarimTutucu(binding)
+    }
+
+    override fun onBindViewHolder(holder: CardTasarimTutucu, position: Int) {
+        val kisi = kisilerListesi[position]
+        val t = holder.tasarim
+
+        t.textViewKisiAd.text = kisi.kisi_ad
+        t.textViewKisiTel.text = kisi.kisi_tel
+
+        t.cardViewSatir.setOnClickListener {
+            val gecis = AnasayfaFragmentDirections.kisiDetayGecis(kisi)
+            Navigation.findNavController(it).navigate(gecis)
+        }
+
+        t.silImage.setOnClickListener {
+            Snackbar.make(it, "${kisi.kisi_ad} silinsin mi?", Snackbar.LENGTH_SHORT)
+                .setAction("Evet") {
+                    sil(kisi.kisi_id)
+                }
+                .show()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return kisilerListesi.size
+    }
+
+    private fun sil(kisi_id: Int) {
+        Log.e("Kisi sil", kisi_id.toString())
+    }
+}
--- app/src/main/java/dev/cagrisayir/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
+++ app/src/main/java/dev/cagrisayir/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
@@ -1,15 +1,20 @@
 package dev.cagrisayir.kisileruygulamasi.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import dev.cagrisayir.kisileruygulamasi.R
 import dev.cagrisayir.kisileruygulamasi.data.entity.Kisiler
 import dev.cagrisayir.kisileruygulamasi.databinding.FragmentAnasayfaBinding
+import dev.cagrisayir.kisileruygulamasi.ui.adapter.KisilerAdapter
 
 class AnasayfaFragment : Fragment() {
     private lateinit var binding: FragmentAnasayfaBinding
@@ -22,18 +27,42 @@ class AnasayfaFragment : Fragment() {
         binding = FragmentAnasayfaBinding.inflate(inflater, container, false)
         binding.toolbarAnasayfa.title = "Anasayfa"
         binding.toolbarAnasayfa.setTitleTextColor(getResources().getColor(R.color.white))
+
+        val kisilerListesi = ArrayList<Kisiler>()
+        val k1 = Kisiler(1, "Ahmet", "11111")
+        val k2 = Kisiler(2, "Ayse", "22222")
+        val k3 = Kisiler(3, "Omer", "333333")
+        kisilerListesi.add(k1)
+        kisilerListesi.add(k2)
+        kisilerListesi.add(k3)
+
+        val kisilerAdapter = KisilerAdapter(requireContext(), kisilerListesi)
+
+        binding.rv.layoutManager = LinearLayoutManager(requireContext())
+        binding.rv.adapter = kisilerAdapter
+
         binding.fab.setOnClickListener {
             val navController = Navigation.findNavController(it)
             navController.navigate(R.id.kisiKayitGecis)
         }
 
-        binding.buttonDetay.setOnClickListener {
-            val kisi = Kisiler(1, "Ahmet", "1111")
-            val gecis = AnasayfaFragmentDirections.kisiDetayGecis(kisi)
-            Navigation.findNavController(it).navigate(gecis)
-        }
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String): Boolean {
+                ara(newText)
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                ara(query)
+                return true
+            }
+        })
 
         return binding.root
+    }
+
+    private fun ara(aramaKelimesi: String) {
+        Log.e("Kisi ara", aramaKelimesi)
     }
 
 }
